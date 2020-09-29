b7fba6b792065738018172230e83420ce507e326
AntoineLibert <duncan-arrakis@hotmail.fr>
2020-09-29 18:04:57 +0200

    Add Entity Class
    Manage the several methods of enTT Entity

---
+++ src/Scene/Entity.cpp
@@ -0,0 +1,9 @@
+#include "Entity.hpp"
+
+
+Entity::Entity(entt::entity handle, Scene* scene, const std::string& name, const TransformComp& transform)
+	: m_EntityHandle(handle), m_Scene(scene)
+{
+	AddComponent<TransformComp>();
+	AddComponent<TagComp>((name.empty()) ? "Entity" : name);
+}
