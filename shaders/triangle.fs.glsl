da869dac5d45c575da6825f54cca83fa0d46fdab
AntoineLibert <duncan-arrakis@hotmail.fr>
2020-09-29 18:08:54 +0200

    Edit Shaders for ECS

--- shaders/triangle.fs.glsl
+++ shaders/triangle.fs.glsl
@@ -1,9 +1,7 @@
 #version 330
 
-in vec3 vColor;
-
-out vec3 fFragColor;
+out vec4 fFragColor;
 
 void main() {
-    fFragColor = vColor;
+    fFragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
 }
--- shaders/triangle.vs.glsl
+++ shaders/triangle.vs.glsl
@@ -1,11 +1,7 @@
 #version 330
 
-layout(location = 0) in vec2 aVertexPosition;
-layout(location = 1) in vec3 aVertexColor;
-
-out vec3 vColor;
+layout(location = 0) in vec4 aVertexPosition;
 
 void main() {
-    vColor = aVertexColor;
-    gl_Position = vec4(aVertexPosition, 0, 1);
+    gl_Position = aVertexPosition;
 }
