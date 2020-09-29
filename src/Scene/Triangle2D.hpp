ea3f4050e076b41542620b9a48e4bdf5cc3a7e12
AntoineLibert <duncan-arrakis@hotmail.fr>
2020-09-29 18:07:51 +0200

    Add Triangle2D class herited from Mesh
    Manage the drawing of a 2d triangle

---
+++ src/Scene/Triangle2D.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "Scene/Mesh.hpp"
+#include "Scene/Components.hpp"
+#include "Scene/Scene.hpp"
+#include <entt.hpp>
+
+class Triangle2D : public Mesh
+{
+public:
+	Triangle2D() = default;
+	Triangle2D(entt::entity handle,
+		Scene* scene,
+		const std::string& name = std::string(),
+		const TransformComp& transform = TransformComp(),
+		const SpriteColorComp& color = SpriteColorComp()
+	);
+};
