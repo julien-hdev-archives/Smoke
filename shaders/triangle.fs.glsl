14c830bd3ac83366c27733d5a6395e7e3246bcfa
Jules Fouchy <jules.fouchy@ntymail.com>
2021-01-11 17:22:23 +0100

    Remove test shaders

--- shaders/triangle.fs.glsl
+++
@@ -1,7 +0,0 @@
-#version 330
-
-out vec4 fFragColor;
-
-void main() {
-    fFragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
-}
--- shaders/triangle.vs.glsl
+++
@@ -1,7 +0,0 @@
-#version 330
-
-layout(location = 0) in vec4 aVertexPosition;
-
-void main() {
-    gl_Position = aVertexPosition;
-}
