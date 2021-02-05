4f9a2dfa89688dc898e65d5b615a9ef93a68bbf1
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-05 02:27:11 +0100

    [Scene] Remove unused members and methods

--- src/Scene/Scene.cpp
+++ src/Scene/Scene.cpp
@@ -4,42 +4,20 @@
 #include "Scene/Mesh.hpp"
 #include "Scene/Triangle2D.hpp"
 
-#include <glm/glm.hpp>
-#include <QtGui/QOpenGLFunctions>
-
-#include "Renderer/Renderer.hpp"
-
-	Scene::Scene()
-	{
-	}
+Scene::Scene()
+{
+}
  
-	Entity Scene::CreateEntity(const std::string& name, const TransformComp& transform)
-	{
-		Entity entity = { m_Registry.create(), this, name, transform };
-		return entity;
-	}
+Entity Scene::CreateEntity(const std::string& name, const TransformComp& transform)
+{
+	Entity entity = { m_Registry.create(), this, name, transform };
+	return entity;
+}
 
     
-	Triangle2D Scene::CreateTriangle2D(const std::string& name, const TransformComp& transform, glm::vec4 color)
-    {
+Triangle2D Scene::CreateTriangle2D(const std::string& name, const TransformComp& transform, glm::vec4 color)
+{
         
-		Triangle2D triangle2D = { m_Registry.create(), this, name, transform, SpriteColorComp(color) };
-		return triangle2D;
-	}
-
-	void Scene::onUpdate()
-	{
-		_camera.update();
-	}
-
-	void Scene::onMousePress() {
-		_camera.onWheelDown();
-	}
-
-	void Scene::onMouseRelease() {
-		_camera.onWheelUp();
-	}
-
-	void Scene::onWheelScroll(float delta) {
-		_camera.onWheelScroll(delta);
-	}
+	Triangle2D triangle2D = { m_Registry.create(), this, name, transform, SpriteColorComp(color) };
+	return triangle2D;
+}
--- src/Scene/Scene.hpp
+++ src/Scene/Scene.hpp
@@ -20,16 +20,9 @@
 
 		Entity CreateEntity(const std::string& name = std::string(), const TransformComp& transform = TransformComp());
 		Triangle2D CreateTriangle2D(const std::string& name = std::string(), const TransformComp& transform = TransformComp(), glm::vec4 color = glm::vec4({ 8.0f, 2.0f, 3.0f, 1.0f }) );
-		
-		void onUpdate();
-		void onMousePress();
-		void onMouseRelease();
-		void onWheelScroll(float delta);
 
 	private:
 		entt::registry m_Registry;
-		SdfRenderer _sdfRenderer;
-		Camera _camera;
 
 		friend class Entity;
 		friend class Mesh;
