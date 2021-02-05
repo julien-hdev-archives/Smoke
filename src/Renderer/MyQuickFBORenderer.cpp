80bc45eff9fb6cf3fa65c8d2bcc39bd997cd6d98
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-05 02:03:54 +0100

    [MyQuickFBORenderer] Move code to the .cpp

--- src/Renderer/MyQuickFBORenderer.cpp
+++ src/Renderer/MyQuickFBORenderer.cpp
@@ -1,1 +1,30 @@
-#pragma once
+#include "MyQuickFBORenderer.hpp"
+
+#include "MyQuickFBO.hpp"
+#include <QtQuick/QQuickWindow>
+#include <QtGui/QOpenGLFunctions>
+#include <QtGui/QOpenGLFramebufferObject>
+
+void MyQuickFBORenderer::synchronize(QQuickFramebufferObject* item) {
+    MyQuickFBO* quickFBO = static_cast<MyQuickFBO*>(item);
+    _window = item->window();
+    quickFBO->camera().update();
+    _cameraInfos = quickFBO->camera().getInfos();
+    _sdfRendererParams = quickFBO->sdfRenderer_Params();
+}
+
+void MyQuickFBORenderer::render() {
+    QOpenGLFunctions* functions = QOpenGLContext::currentContext()->functions();
+    functions->glClearColor(1.f, 0.f, 0.f, 1.f);
+    functions->glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+    _sdfRenderer.render(_cameraInfos, _sdfRendererParams);
+    update();
+    _window->resetOpenGLState();
+}
+
+QOpenGLFramebufferObject* MyQuickFBORenderer::createFramebufferObject(const QSize& size) {
+    QOpenGLFramebufferObjectFormat format;
+    format.setAttachment(QOpenGLFramebufferObject::CombinedDepthStencil);
+    return new QOpenGLFramebufferObject(size, format);
+}
--- src/Renderer/MyQuickFBORenderer.hpp
+++ src/Renderer/MyQuickFBORenderer.hpp
@@ -1,12 +1,10 @@
 #pragma once
 
 #include <QtQuick/QQuickFramebufferObject>
-#include <QtQuick/QQuickWindow>
-#include <QtGui/QOpenGLFunctions>
-// #include <QtGui/QOpenGLContext>
-#include <QtGui/QOpenGLFramebufferObject>
 #include "SdfRenderer.hpp"
 #include "SdfRenderer_Params.hpp"
+#include "Camera/CameraInfos.hpp"
+class QOpenGLFramebufferObject;
 
 class MyQuickFBORenderer : public QQuickFramebufferObject::Renderer {
 
@@ -14,33 +12,13 @@ public:
     MyQuickFBORenderer() = default;
     ~MyQuickFBORenderer() = default;
 
-    void synchronize(QQuickFramebufferObject* item) override {
-        MyQuickFBO* quickFBO = static_cast<MyQuickFBO*>(item);
-        _window = item->window();
-        quickFBO->camera().update();
-        _cameraInfos = quickFBO->camera().getInfos();
-        _sdfRendererParams = quickFBO->sdfRenderer_Params();
-    }
-
-    void render() override {
-        QOpenGLFunctions* functions = QOpenGLContext::currentContext()->functions();
-        functions->glClearColor(1.f, 0.f, 0.f, 1.f);
-        functions->glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-
-        _sdfRenderer.render(_cameraInfos, _sdfRendererParams);
-        update();
-        _window->resetOpenGLState();
-    }
-
-    QOpenGLFramebufferObject* createFramebufferObject(const QSize& size) override {
-        QOpenGLFramebufferObjectFormat format;
-        format.setAttachment(QOpenGLFramebufferObject::CombinedDepthStencil);
-        return new QOpenGLFramebufferObject(size, format);
-    }
+    void synchronize(QQuickFramebufferObject* item) override;
+    void render() override;
+    QOpenGLFramebufferObject* createFramebufferObject(const QSize& size) override;
 
 private:
     SdfRenderer _sdfRenderer;
-    CameraInfos _cameraInfos;
     SdfRenderer_Params _sdfRendererParams;
+    CameraInfos _cameraInfos;
     QQuickWindow* _window;
 };
