dfc4c7dfc091b27ce61d3e9dd24a2de031e8c3f7
Julien-Haudegond <44610840+Julien-Haudegond@users.noreply.github.com>
2021-02-12 13:07:25 +0100

    [Utility] ObjectListModel creation

---
+++ src/Utility/ObjectListModel.cpp
@@ -0,0 +1,33 @@
+#include "ObjectListModel.hpp"
+
+int
+ObjectListModel::rowCount(const QModelIndex &parent = QModelIndex()) const
+{
+    return _data.size();
+}
+
+QVariant
+ObjectListModel::data(const QModelIndex &index,
+                      int role = Qt::DisplayRole) const
+{
+    if (!hasIndex(index.row(), index.column(), index.parent())) return {};
+
+    auto obj = _data.at(index.row());
+    return QVariant::fromValue<QObject *>(obj);
+}
+
+Qt::ItemFlags
+ObjectListModel::flags(const QModelIndex &index) const
+{
+    Qt::ItemFlags f = QAbstractListModel::flags(index);
+    if (index.isValid()) f |= Qt::ItemIsEditable;
+    return f;
+}
+
+void
+ObjectListModel::populate(const QObjectList &objects)
+{
+    beginResetModel();
+    _data = objects;
+    endResetModel();
+}
---
+++ src/Utility/ObjectListModel.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <QAbstractListModel>
+
+/// \brief Static ObjectListModel used for QML Views.
+/// \todo Make it dynamic.
+class ObjectListModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+  public:
+    explicit ObjectListModel(QObject *parent = nullptr)
+        : QAbstractListModel(parent)
+    {}
+    ~ObjectListModel() = default;
+
+    // ==============================
+    // Virtual methods needed for the
+    // ListModel to work properly.
+    // ==============================
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index,
+                  int role = Qt::DisplayRole) const override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
+
+    // ==============================
+    // Internal methods used to work directly with the data.
+    // WARNING: be careful to insert/removeRows()
+    // in case of dynamic object lists.
+    // ==============================
+
+    /// \brief Clear and populate the data with a list of QObjects.
+    /// \param objects List of QObjects to store as data.
+    /// \todo Check if handling parent ownership is needed or not.
+    void populate(const QObjectList &objects);
+
+    /// \brief Returns the Object at the corresponding index.
+    /// \param index Index in the list.
+    inline const QObject *at(const size_t index) const
+    {
+        return _data.at(index);
+    }
+
+  private:
+    /// Internal list of QObjects.
+    QObjectList _data;
+};
