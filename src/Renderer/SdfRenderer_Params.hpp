2cfa461ce6a6f5019fadedbfe93faa5385de1e5e
Julien-Haudegond <44610840+Julien-Haudegond@users.noreply.github.com>
2021-02-15 14:17:19 +0100

    [Renderer] SdfRendererProperties class (base)
    - To improve creating a subclass of ObjectListModel for Attributes.

--- src/Renderer/SdfRenderer_Params.hpp
+++ src/Renderer/SdfRenderer_Params.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "Core/FloatAttribute.hpp"
+#include "Utility/ObjectListModel.hpp"
+
+#include <QObject>
+
 struct SdfRenderer_Params
 {
     float absorptionCoefficient = 0.5f;
@@ -9,4 +14,36 @@ struct SdfRenderer_Params
     int maxVolumeLightMarchSteps = 4;
     int maxSdfSphereSteps = 15;
     int maxOpaqueShadowMarchSteps = 10;
+};
+
+class SdfRendererProperties : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(ObjectListModel *attributes MEMBER _attributes NOTIFY
+                   attributesChanged)
+  public:
+    SdfRendererProperties(QObject *parent = nullptr) : QObject(parent)
+    {
+        SdfRenderer_Params initialParams;
+        QObjectList objList;
+
+        _attributes = new ObjectListModel(this);
+
+        // Initialize each attribute.
+        // Only one for now, for the test.
+        auto absorptionCoefficient = new FloatAttribute(
+            "absorptionCoefficient", "Absorption Coefficient",
+            initialParams.absorptionCoefficient, 0.f, 1.f, 0.01f, _attributes);
+        objList.append(absorptionCoefficient);
+
+        // Populate the ListModel.
+        _attributes->populate(objList);
+    }
+    ~SdfRendererProperties() = default;
+
+  public:
+    Q_SIGNAL void attributesChanged();
+
+  private:
+    ObjectListModel *_attributes;
 };
